75d8d116f7142f545c67452eb5719a9924b356e9
Raunak Rahi <70812000+RaunakRahi@users.noreply.github.com>
2024-04-18 14:43:10 +0000

    container config

--- .devcontainer/basics/variables.js
+++ .devcontainer/basics/variables.js
@@ -2,9 +2,10 @@ const accountId = 14455
 let acountEmail = "kraunak1110@gmail.com"
 var accountPassword = "1234"
 accountCity = "Samastipur"
+let accountState;
 //accountId = 2
 accountEmail="xyz@gmail"
 accountPassword ="54789632"
 accountCity ="Benguluru"
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
