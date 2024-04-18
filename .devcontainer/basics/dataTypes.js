9c146b9894358a6dbbdc9813ed9e715606bb1bde
Raunak Rahi <70812000+RaunakRahi@users.noreply.github.com>
2024-04-18 15:04:12 +0000

    dataTypes

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+//alert()
+console.log("Raunak")
+let name = "Raunak"
+let age = 18
+let isLoggedIn = false
+console.log(typeof "Raunak");
+console.log(typeof null);
+console.log(typeof undefined);
